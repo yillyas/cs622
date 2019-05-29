2073c33f6946d563d4d1467b9e99ae1fa2e65006
yasirilyas <yasirilyas@192.168.0.21>
2019-05-28 23:32:51 -0400

    corrected formatting and added additional comments.

--- CS622_Assignment2_CarRent_Illyas/src/carRent/VehicleRepository.java
+++ CS622_Assignment2_CarRent_Illyas/src/carRent/VehicleRepository.java
@@ -3,11 +3,14 @@ package carRent;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+/**
+ * Used to keep track of different objects during the application execution.
+ * Will be replaced by database in future.
+ * @author yasirilyas
+ */
 public class VehicleRepository {
 	
 	public static LinkedList<Vehicle> allVehicle = new LinkedList<Vehicle>();
 	public static HashMap<Integer, LinkedList<Vehicle>> vehicleByZip = new HashMap<Integer, LinkedList<Vehicle>>();
 	public static HashMap<String, LinkedList<Vehicle>> bookedVehicle = new HashMap<String, LinkedList<Vehicle>>();
-	//public static HashMap<Integer, LinkedList<Vehicle>> bookingDetail = new HashMap<Integer, LinkedList<Vehicle>>();
-
 	}
